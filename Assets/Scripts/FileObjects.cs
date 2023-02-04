ec686877dc710efb34f8133a936dad54d6fc00d8
Manuel Gorostiaga <gorostiagamanuel@gmail.com>
2023-02-04 08:53:34 -0300

    contains and cut

--- Assets/Scripts/FileObjects.cs
+++ Assets/Scripts/FileObjects.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using System.Collections.Generic;
 using System;
 using System.IO;
+using System.Linq;
+using Unity.VisualScripting;
 using Object = System.Object;
 
 [System.Serializable]
@@ -11,22 +13,55 @@ public class OSFolder: Object
     public OSFolder(string path, string name)
     {
         folderPath = path+"/"+name;
-        folderName = name;
+        this.name = name;
         subfolders = new List<OSFolder>();
         files = new List<OSFile>();
     }
     [NonSerialized] public string folderPath;
     [NonSerialized] public OSFolder ParentFolder;
-    public string folderName;
+    public string name;
     public List<OSFolder> subfolders;
     public List<OSFile> files;
-   
+
+    public bool Contains(string fileName)
+    {
+        foreach (var folder in subfolders)
+        {
+            if (folder.name == fileName)
+            {
+                return true;
+            }
+        }
+
+        foreach (var file in files)
+        {
+            if (file.name == fileName)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Object Cut(string fileName)
+    {
+        foreach (var file in  files)
+        {
+            if (file.name == fileName)
+            {
+                files.Remove(file);
+                return file;
+            }
+        }
+        return null;
+    }
 }
 
 [System.Serializable]
 public class OSFile : Object
 {
-    public string fileName;
-    public string targetFolderPath;
+    public string name;
+    [NonSerialized] public string targetFolderPath;
 }
 
