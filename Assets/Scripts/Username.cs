70ff043132b9bf294df6ad157825b39a0aa1feee
Robertino Papeschi <roberpapeschi@gmail.com>
2023-02-04 20:49:41 -0300

    menu

--- Assets/Scripts/Username.cs
+++ Assets/Scripts/Username.cs
@@ -5,13 +5,11 @@ using TMPro;
 public class Username : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI nameText, idText,emailText;
-    [SerializeField] GameObject namePanel, menuPanel;
     public void SaveName()
     {
         PlayerPrefs.SetString("Name", nameText.text.ToUpper());
         idText.text = PlayerPrefs.GetString("Name");
         emailText.text = PlayerPrefs.GetString("Name").ToLower()+ "@gmail.com";
-        namePanel.SetActive(false);
-        menuPanel.SetActive(true);
+
     }
 }
